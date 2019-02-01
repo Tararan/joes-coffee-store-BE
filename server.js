9ad47c3592a04a3408f6740cb0a2bbebeafcd0d1
Tararan <30859637+Tararan@users.noreply.github.com>
2019-02-01 08:58:16 +0100

    finished with setting up a server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const knex = require('knex')
 
 const database = knex({
@@ -19,7 +20,23 @@ app.use(bodyParser.json());
 
 database.select('*').from('products').then(data => {
     app.get('/', (req, res) => { 
+        // console.log(res);
+        // console.log(data);
         res.send(data);
+        // const properFormatData = data;
+        const fileLocation = '../joes-coffee-store/src/Products/productsContent.js';
+        // const fileLocation = '../joes-coffee-store/src/Products/fetchProducts.js';
+        const properFormatData = JSON.stringify(data);
+        // const productsContentData =  `export const fetchContent = ${properFormatData};`;
+        const productsContentData = `export const productsContent = ${properFormatData};`;
+        // res.send(JSON.stringify(data));
+        fs.writeFile(fileLocation, productsContentData, 'utf-8');
+        /* fs.readFile(fileLocation, function (err, data) {
+            if(err) return console.log(err);
+           const idReplace = data.replace(`{"id":`, `id:`);
+           fs.writeFile(fileLocation, idReplace);
+           console.log(idReplace);
+        }); */
     });
 });
 
